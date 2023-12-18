9d96290c17eaeccdad4b5fa64dc42789a6d2f359
Yashraj Raj <147085086+yv18@users.noreply.github.com>
2023-12-18 13:35:45 -0500

    Added JavaScript

---
+++ Script/index.js
@@ -0,0 +1,10 @@
+const btn1 = document.getElementById("btn1");
+const btn2 = document.getElementById("btn2");
+
+btn1.addEventListener("click", function() {
+    window.location.href = "https://yv18.github.io/CloneAmazon/";
+});
+
+btn2.addEventListener("click", function() {
+    window.location.href = "https://yv18.github.io/TaskManager/";
+});
