4a28a7b94d117c82cd7137eeb77a28f4cf956f27
Yashraj Raj <147085086+yv18@users.noreply.github.com>
2023-12-22 18:28:59 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const btn1 = document.getElementById("btn1");
 const btn2 = document.getElementById("btn2");
+const btn2 = document.getElementById("btn3");
 
 btn1.addEventListener("click", function() {
     window.location.href = "https://yv18.github.io/CloneAmazon/";
@@ -7,4 +8,8 @@ btn1.addEventListener("click", function() {
 
 btn2.addEventListener("click", function() {
     window.location.href = "https://yv18.github.io/TaskManager/";
+});
+
+btn3.addEventListener("click", function() {
+    window.location.href = "https://github.com/yv18/nike";
 });
