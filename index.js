d85b5f91c8fe9d767d2d1fc246912347d26ae636
Yashraj Raj <147085086+yv18@users.noreply.github.com>
2023-12-22 18:40:45 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const btn1 = document.getElementById("btn1");
 const btn2 = document.getElementById("btn2");
-const btn2 = document.getElementById("btn3");
+const btn3 = document.getElementById("btn3");
 
 btn1.addEventListener("click", function() {
     window.location.href = "https://yv18.github.io/CloneAmazon/";
