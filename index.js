e3a1e18cf59d312b4e6542efd880c1c334fc56d4
Yashraj Raj <147085086+yv18@users.noreply.github.com>
2023-12-28 14:02:11 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const btn1 = document.getElementById("btn1");
 const btn2 = document.getElementById("btn2");
 const btn3 = document.getElementById("btn3");
+const btn6 = document.getElementById("btn6");
+const btn4 = document.getElementById("btn4");
+const btn5 = document.getElementById("btn5");
 
 btn1.addEventListener("click", function() {
     window.location.href = "https://yv18.github.io/CloneAmazon/";
@@ -14,9 +17,7 @@ btn3.addEventListener("click", function() {
     window.location.href = "https://github.com/yv18/nike";
 });
 
-const btn6 = document.getElementById("btn6");
-const btn4 = document.getElementById("btn4");
-const btn5 = document.getElementById("btn5");
+
 btn4.addEventListener("click", function() {
     window.location.href = "https://yv18.github.io/DriveTest/";
 });
